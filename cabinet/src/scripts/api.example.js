f6f5a61f694eb0af374290c41b753ea27f911ba4
dmitrisi4 <sichkardimitri@gmail.com>
2023-08-05 17:00:39 +0200

    fix api.example, gitignore

--- cabinet/src/scripts/api.example.js
+++ cabinet/src/scripts/api.example.js
@@ -5,4 +5,4 @@ const REGISTER_URL = `${API_URL}api/auth/registration`;
 const ORDERS_URL = `${API_URL}api/order/`;
 const CONTACTS_URL = `${API_URL}api/contacts/`;
 
-export {LOGIN_URL, ORDERS_URL, CONTACTS_URL};
+export {LOGIN_URL, ORDERS_URL, CONTACTS_URL, REGISTER_URL};
