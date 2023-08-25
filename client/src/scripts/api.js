32d7de852c6810a76d9aaad03d0e24dc38fe06e4
dmitrisi4 <sichkardimitri@gmail.com>
2023-08-25 12:53:06 +0200

    rebuild with landing parameters

---
+++ client/src/scripts/api.js
@@ -0,0 +1,6 @@
+import { keys } from "../../../env";
+const API_URL = keys.api_url;
+const ORDERS_URL = `${API_URL}api/order/`;
+const CONTACTS_URL = `${API_URL}api/contacts/`;
+
+export {ORDERS_URL, CONTACTS_URL};
