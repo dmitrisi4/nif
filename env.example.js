2dabef097f1d1f01ee8f8c44197ad2884624bf52
dmitrisi4 <sichkardimitri@gmail.com>
2023-10-11 00:28:12 +0200

    upd

--- env.example.js
+++ env.example.js
@@ -2,15 +2,14 @@ module.exports.dbConfig = {
   HOST: "127.0.0.1",
   USER: "root",
   PASSWORD: "",
-  DB: "nifdb_orders",
+  DB: "nif_db",
   dialect: "mysql",
 };
 
 module.exports.keys = {
 	jwt: 'dev-jwt',
-	cors: 'http://localhost:1234',
-  cors2: 'http://localhost:60743',
+	cors: 'http://si4kard-nif-cabinet-front.s3-website.eu-north-1.amazonaws.com',
+  cors2: 'http://si4kard-nif-font.s3-website.eu-north-1.amazonaws.com',
   port: process.env.PORT || 4000,
-  api_url: 'http://localhost:4000/'
+  api_url: 'http://13.53.206.241:4000/'
 };
-
